356ec335547d7aff2a336f5c4b5320534867b056
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-23 17:38:25 +0100

    base

---
+++ src/main/java/daw2a/gestion_alimentos_api_rest/GestionAlimentosApiRestApplication.java
@@ -0,0 +1,13 @@
+package daw2a.gestion_alimentos_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAlimentosApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAlimentosApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daw2a/gestion_alimentos_api_rest/GestionAlimentosApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2a.gestion_alimentos_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAlimentosApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
