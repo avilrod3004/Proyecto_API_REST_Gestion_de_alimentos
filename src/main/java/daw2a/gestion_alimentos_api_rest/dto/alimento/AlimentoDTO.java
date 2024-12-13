b8c1b0202cd6b2f9670dd30af2211cf63dee9927
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-13 18:41:06 +0100

    dtos de alimento. para crear y para consultar

---
+++ src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/AlimentoDTO.java
@@ -0,0 +1,10 @@
+package daw2a.gestion_alimentos_api_rest.dto.alimento;
+
+import java.time.LocalDate;
+
+public class AlimentoDTO {
+    private String nombre;
+    private String tipo;
+    private String estado;
+    private LocalDate fechaCaducidad;;
+}
---
+++ src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/CrearAlimentoDTO.java
@@ -0,0 +1,9 @@
+package daw2a.gestion_alimentos_api_rest.dto.alimento;
+
+import java.time.LocalDate;
+
+public class CrearAlimentoDTO {
+    private String nombre;
+    private String tipo;
+    private LocalDate fechaCaducidad;
+}
