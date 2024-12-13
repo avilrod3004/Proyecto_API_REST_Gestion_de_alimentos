47a5034021b7244fb884e525a98accededf9267e
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-13 19:55:00 +0100

    correcciones en los dtos

--- src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/AlimentoDTO.java
+++ src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/AlimentoDTO.java
@@ -1,10 +1,14 @@
 package daw2a.gestion_alimentos_api_rest.dto.alimento;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class AlimentoDTO {
+    private Long id;
     private String nombre;
     private String tipo;
     private String estado;
-    private LocalDate fechaCaducidad;;
+    private LocalDate fechaCaducidad;
 }
--- src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/CrearAlimentoDTO.java
+++ src/main/java/daw2a/gestion_alimentos_api_rest/dto/alimento/CrearAlimentoDTO.java
@@ -1,7 +1,10 @@
 package daw2a.gestion_alimentos_api_rest.dto.alimento;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class CrearAlimentoDTO {
     private String nombre;
     private String tipo;
