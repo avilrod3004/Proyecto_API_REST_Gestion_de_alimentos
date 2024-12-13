37392a86ebb8ff365f6f6a2eb6ba2a109953e6a0
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-13 19:55:49 +0100

    servicio y controlador de alimentos basico

---
+++ src/main/java/daw2a/gestion_alimentos_api_rest/controllers/AlimentoController.java
@@ -0,0 +1,78 @@
+package daw2a.gestion_alimentos_api_rest.controllers;
+
+import daw2a.gestion_alimentos_api_rest.dto.alimento.AlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.dto.alimento.CrearAlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.dto.alimento.ModificarAlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.services.AlimentoService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alimentos")
+public class AlimentoController {
+    private final AlimentoService alimentoService;
+
+    public AlimentoController(AlimentoService alimentoService) {
+        this.alimentoService = alimentoService;
+    }
+
+    /**
+     * Listar todos los alimentos o por nombre
+     * @param nombre Nombre que deben contener los alimentos
+     * @param pageable Pageable
+     * @return Listado de alimentos
+     */
+    @GetMapping
+    public ResponseEntity<Page<?>> listarAlimentos(@RequestParam(required = false) String nombre, Pageable pageable) {
+        Page<?> alimentos = alimentoService.listarAlimentos(nombre, pageable);
+        return ResponseEntity.ok(alimentos);
+    }
+
+    /**
+     * Obtener los detalles de un alimento
+     * @param id Identificador
+     * @return Alimento
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<AlimentoDTO> obtenerAlimento(@PathVariable Long id) {
+        AlimentoDTO alimento = alimentoService.obtenerAlimento(id);
+        return ResponseEntity.ok(alimento);
+    }
+
+    /**
+     * Guardar un alimento nuevo
+     * @param crearAlimentoDTO DTO con los datos necesarios
+     * @return Alimento guardado
+     */
+    @PostMapping
+    public ResponseEntity<AlimentoDTO> guardarAlimento(@RequestBody @Valid CrearAlimentoDTO crearAlimentoDTO) {
+        AlimentoDTO nuevoAlimento = alimentoService.crearAlimento(crearAlimentoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoAlimento);
+    }
+
+    /**
+     * Actualizar los datos de un alimento existente
+     * @param id Identificador de alimento
+     * @param modificarAlimentoDTO Datos nuevos
+     * @return Alimento actualizado
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<AlimentoDTO> actualizarAlimento(@PathVariable Long id, @RequestBody @Valid ModificarAlimentoDTO modificarAlimentoDTO) {
+        return ResponseEntity.ok(alimentoService.actualizarAlimento(id, modificarAlimentoDTO));
+    }
+
+    /**
+     * Eliminar un alimento
+     * @param id Identificador del alimento
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarAlimento(@PathVariable Long id) {
+        alimentoService.eliminarAlimento(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/daw2a/gestion_alimentos_api_rest/services/AlimentoService.java
@@ -0,0 +1,120 @@
+package daw2a.gestion_alimentos_api_rest.services;
+
+import daw2a.gestion_alimentos_api_rest.dto.alimento.AlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.dto.alimento.CrearAlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.dto.alimento.ModificarAlimentoDTO;
+import daw2a.gestion_alimentos_api_rest.entities.Alimento;
+import daw2a.gestion_alimentos_api_rest.exceptions.RecursoNoEncontradoException;
+import daw2a.gestion_alimentos_api_rest.repositories.AlimentoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlimentoService {
+    private final AlimentoRepository alimentoRepository;
+
+    public AlimentoService(AlimentoRepository alimentoRepository) {
+        this.alimentoRepository = alimentoRepository;
+    }
+
+    /**
+     * Listar todos los alimentos o buscar por nombre
+     * @param nombre Nombre del alimento
+     * @param pageable Pageable
+     * @return Listado de alimentos
+     */
+    public Page<?> listarAlimentos(String nombre, Pageable pageable) {
+        Page<Alimento> alimentos;
+
+        if (nombre != null && !nombre.isEmpty()) {
+            alimentos = alimentoRepository.findByNombreContainingIgnoreCase(nombre, pageable);
+        } else {
+            alimentos = alimentoRepository.findAll(pageable);
+        }
+
+        return alimentos.map(this::convertirAAlimentoDTO);
+    }
+
+    /**
+     * Obtener los detalles de un alimento identificado por su id
+     * @param id Identificador del alimento
+     * @return DTO con los datos del alimento
+     * @throws RecursoNoEncontradoException Si no hay un alimento con ese id
+     */
+    public AlimentoDTO obtenerAlimento(Long id) {
+        Alimento alimento = alimentoRepository.findById(id)
+                .orElseThrow(() -> new RecursoNoEncontradoException("Alimento con id " + id + " no encontrado"));
+
+        return convertirAAlimentoDTO(alimento);
+    }
+
+    /**
+     * Crear un nuevo alimento
+     * @param crearAlimentoDTO DTO de alimento con los datos necesarios
+     * @return AlimentoDTO
+     */
+    public AlimentoDTO crearAlimento(CrearAlimentoDTO crearAlimentoDTO) {
+        Alimento alimento = new Alimento();
+        alimento.setNombre(crearAlimentoDTO.getNombre());
+        alimento.setTipo(crearAlimentoDTO.getTipo());
+        alimento.setEstado("Cerrado");
+        alimento.setFechaCaducidad(crearAlimentoDTO.getFechaCaducidad());
+
+        return convertirAAlimentoDTO(alimentoRepository.save(alimento));
+    }
+
+    /**
+     * Actualizar los datos de un alimento
+     * @param id Identificador del alimento
+     * @param modificarAlimentoDTO DTO con los datos para modificarlo
+     * @return Alimento DTO
+     * @throws RecursoNoEncontradoException Si no hay un alimento con ese id
+     */
+    public AlimentoDTO actualizarAlimento(Long id, ModificarAlimentoDTO modificarAlimentoDTO) {
+        Alimento alimento = alimentoRepository.findById(id)
+                .orElseThrow(() -> new RecursoNoEncontradoException("Alimento con id " + id + " no encontrado"));
+
+        if (modificarAlimentoDTO.getNombre() != null) {
+            alimento.setNombre(modificarAlimentoDTO.getNombre());
+        }
+        if (modificarAlimentoDTO.getTipo() != null) {
+            alimento.setTipo(modificarAlimentoDTO.getTipo());
+        }
+        if (modificarAlimentoDTO.getEstado() != null) {
+            alimento.setEstado(modificarAlimentoDTO.getEstado());
+        }
+        if (modificarAlimentoDTO.getFechaCaducidad() != null) {
+            alimento.setFechaCaducidad(modificarAlimentoDTO.getFechaCaducidad());
+        }
+
+        return convertirAAlimentoDTO(alimentoRepository.save(alimento));
+    }
+
+    /**
+     * Eliminar un alimento
+     * @param id Identificador del alimento
+     * @throws RecursoNoEncontradoException Si no hay un alimento con ese id
+     */
+    public void eliminarAlimento(Long id) {
+        Alimento alimento = alimentoRepository.findById(id)
+                .orElseThrow(() -> new RecursoNoEncontradoException("Alimento con id " + id + " no encontrado"));
+
+        alimentoRepository.delete(alimento);
+    }
+
+    /**
+     * Convertir una entidad en DTO
+     * @param alimento Entidad de alimento
+     * @return DTO de alimento
+     */
+    public AlimentoDTO convertirAAlimentoDTO(Alimento alimento) {
+        AlimentoDTO alimentoDTO = new AlimentoDTO();
+        alimentoDTO.setId(alimento.getId());
+        alimentoDTO.setNombre(alimento.getNombre());
+        alimentoDTO.setTipo(alimento.getTipo());
+        alimentoDTO.setEstado(alimento.getEstado());
+        alimentoDTO.setFechaCaducidad(alimento.getFechaCaducidad());
+        return alimentoDTO;
+    }
+}
