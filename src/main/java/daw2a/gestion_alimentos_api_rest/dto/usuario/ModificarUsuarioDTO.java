dd91907ad3501c4901d2cd3ff4fe0b9452f795b2
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-14 10:23:37 +0100

    eliminar anotaciones innecesarias

--- src/main/java/daw2a/gestion_alimentos_api_rest/dto/usuario/ModificarUsuarioDTO.java
+++ src/main/java/daw2a/gestion_alimentos_api_rest/dto/usuario/ModificarUsuarioDTO.java
@@ -7,16 +7,12 @@ import lombok.Data;
 
 @Data
 public class ModificarUsuarioDTO {
-    @NotBlank(message = "El nombre del usuario es obligatorio")
     private String nombre;
 
-    @NotBlank(message = "El email es obligatorio")
     @Email
     private String email;
 
-    @NotBlank(message = "La contaseña es obligatoria")
     private String password;
 
-    @Pattern(regexp = "USUARIO|ADMINISTRADOR", message = "El rol debe der 'USUARIO' o 'ADMINISTRADOR'.")
-    private String rol = "USUARIO";
+    private String rol;
 }
